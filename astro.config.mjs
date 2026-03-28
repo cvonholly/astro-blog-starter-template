ca05914ab7893b03038b732c0fa3d9e91fcbce63
Carl von Holly <44812683+cvonholly@users.noreply.github.com>
2026-03-28 10:22:14 +0100

    import latex and fix slider blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 import cloudflare from "@astrojs/cloudflare";
 
@@ -14,4 +16,10 @@ export default defineConfig({
 			enabled: true,
 		},
 	}),
+	markdown: {
+     remarkPlugins: [remarkMath],
+     rehypePlugins: [rehypeKatex],
+   },
 });
+
+
